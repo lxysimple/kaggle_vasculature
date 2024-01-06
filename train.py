265122b17e0f801b1d4465b08dd3c1319402eb89
lxysimple <128936873+lxysimple@users.noreply.github.com>
2024-01-06 14:55:27 +0000

    train_batch_size = 8

--- train.py
+++ train.py
@@ -47,7 +47,7 @@ class CFG:
     image_size = 896 # 896/512/1024  # 图片大小 
     input_size = 896 # 896/512/1024   # 输入尺寸
 
-    train_batch_size = 16  # 训练批量大小
+    train_batch_size = 8 # 16 # 训练批量大小
     valid_batch_size = train_batch_size * 2  # 验证批量大小
     num_workers = 2
 
